4d1e919ac4c8abd9d855e3fa1eef3517048ccc24
Irvin <irvinfly@gmail.com>
2026-01-31 03:41:38 +0800

    fix docker error due to missing jekyll-feed from config.yml

--- in_docker.sh
+++ in_docker.sh
@@ -5,6 +5,7 @@ echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 gem install jekyll bundler
+gem install jekyll-feed
 gem install jekyll-redirect-from
 gem install jekyll-sitemap
 jekyll serve --watch --host 0.0.0.0 --livereload --incremental
